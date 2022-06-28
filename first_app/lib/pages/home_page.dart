9f0c963942577671db5ef63ec57e79ef629574f9
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-06-28 15:40:25 -0300

    Importando e utilizando o english_words

--- first_app/lib/pages/home_page.dart
+++ first_app/lib/pages/home_page.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    
+    final wordPair = WordPair.random();
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text("Nomes para Startup"),
         backgroundColor: Colors.deepPurple[400],
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Hello World"),
+      body: Center(
+        // "PascalCase" - Define que a primeira letra de cada palavra deve ser maiúscula.
+        child: Text(wordPair.asPascalCase),
       ),
     );
   }
